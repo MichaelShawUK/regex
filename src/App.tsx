a97467a6a5071039ec2f80ba7edab7b02ba981de
MichaelShawUK <MichaelShawUK@gmail.com>
2023-08-12 15:16:01 +0100

    Remove logic from App component JSX

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,24 @@ import levelData from "./data/levelData";
 function App() {
   const [level, setLevel] = useState(0);
 
+  const isPrevLevel = level > 0;
+  const isNextLevel = level < levelData.length - 1;
+
+  function prevClickHandler() {
+    setLevel((l) => Math.max(--l, 0));
+  }
+
+  function nextClickHandler() {
+    setLevel((l) => Math.min(++l, levelData.length - 1));
+  }
+
   return (
     <>
       <LevelTemplate levelData={levelData[level]} />
-      {level > 0 && (
-        <button
-          onClick={() => {
-            setLevel((l) => Math.max(--l, 0));
-          }}
-        >
-          Previous Level
-        </button>
+      {isPrevLevel && (
+        <button onClick={prevClickHandler}>Previous Level</button>
       )}
-      {level < levelData.length - 1 && (
-        <button
-          onClick={() => {
-            setLevel((l) => Math.min(++l, levelData.length - 1));
-          }}
-        >
-          Next Level
-        </button>
-      )}
+      {isNextLevel && <button onClick={nextClickHandler}>Next Level</button>}
     </>
   );
 }
